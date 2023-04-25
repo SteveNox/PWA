5680ef943b9c5c809018b584ba8f54cd787c7ee5
Steve Nox <stefan.kubinski@gmail.com>
2023-04-25 21:48:38 +0200

    end of second day

--- web/js/app.js
+++ web/js/app.js
@@ -2,6 +2,49 @@ if('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
         .then(function() {
-            console.log('SERVICE WORKER registerd!');
+            console.log('SERVICE WORKER registered!');
         });
 };
+
+console.log('BEFORE TIMEOUT!')
+
+var promise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('TIMEOUT COMPLETE')
+        //reject('Absichtlicher Fehler')
+    }, 3000);
+});
+
+//Alternative zu .then:
+//muss in async function aufgerufen werden
+async function awaitingTest() {
+    try {
+        var result = await promise;
+        console.log("Erfolg:", result);
+    } catch (error) {
+        console.log(error);   
+    }
+}
+
+awaitingTest();
+
+//Standardpromiseaufruf
+// promise.then(function(text) {
+//     console.log(text);
+// }).catch(function(error) {
+//     console.log(error);
+// })
+
+console.log('AFTER TIMEOUT!');
+
+fetch('http://httpbin.org/ip')
+    .then(function(response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(result) {
+        console.log(result);
+    })
+    .catch(function(error) {
+        console.log("FEHLER->", error);
+    });
--- web/sw.js
+++ web/sw.js
@@ -1,47 +1,62 @@
-self.addEventListener('install', function(event) {
-    // var cache = await caches.open('static');
-    // cache.add('/js/app.js');
-    // console.log('->added something to cache');
-    event.waitUntil(
-        caches.open('static')
-            .then(function(cache) {
-                cache.add('/');    
-                cache.add('index.html');    
-                cache.add('/js/app.js');
-                console.log('->added something to cache');
-            })
-    )
+const STATIC_CACHE_NAME = 'static-v2';
+const DYNAMIC_CACHE_NAME = 'dynamic-v2';
+
+self.addEventListener('install', async function(event) {
+    console.log('[SW]: Service worker installing..., hopefully!', event);
+    //abwarten, bis der gesamte teil innerhalb waitUntil fertig ist
+    //sonst kann fetch möglicherweise noch nicht auf den cache zugreifen
+    // event.waitUntil(
+    //     caches.open('static')
+    //         .then(function(staticCache) {
+    //             staticCache.add('/');
+    //             staticCache.add('index.html');
+    //             staticCache.add('app.js');
+    //         })
+    // );
+
+    var staticCache = await caches.open(STATIC_CACHE_NAME);
+    await staticCache.add('/');
+    await staticCache.add('index.html');
+    await staticCache.add('/js/app.js');
+
     //do not wait for page-reload
+    //erübrigt die manuelle aktivierung eines neuen serviceworkers
     return self.skipWaiting();
 });
 
 self.addEventListener('activate', function(event) {
+    //console.log('[SW]: Service worker activating...', event);
     //ask the client to take control over the service worker
     return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
+    caches.keys()
+        .then(function(cacheEntries) {
+            console.log(cacheEntries);
+        });
+        
+     event.respondWith(
         caches.match(event.request)
-            .then(function(resFromCache) {
-                if(resFromCache) {
-                    return resFromCache;
+            .then(function(cacheResponse) {
+                if(cacheResponse) {
+                    //treffer im cache gefunden->zurückgeben
+                    return cacheResponse;
                 } else {
+                    //treffer nicht gefunden->internet fragen
                     return fetch(event.request)
-                        .then(function(resFromInternet) {
-                            return caches.open('dynamic')
-                                .then(function(cache) {
-                                    cache.put(event.request.url, resFromInternet.clone())
-                                    return resFromInternet;
-                                });
-                        });
+                        .then(function(responseFromInternet) {
+                            return caches.open(DYNAMIC_CACHE_NAME)
+                                .then(function(dynamicCache) {
+                                    dynamicCache.put(event.request.url, responseFromInternet.clone());
+                                    return responseFromInternet;
+                                })
+                        })
                 }
-            }
-        )
-    )
+            })
+     )
 });
 
 self.addEventListener('sync', function(event) {
-    console.log('[SW]: Service worker syncing...', event);
-
+    //console.log('[SW]: Service worker syncing...', event);
 });
