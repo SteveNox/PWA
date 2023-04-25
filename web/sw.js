97489f292bc2fc40a94223da068fb59486ea0fdf
Steve Nox <stefan.kubinski@gmail.com>
2023-04-25 17:37:57 +0200

    initial work for day2

--- web/sw.js
+++ web/sw.js
@@ -1,19 +1,47 @@
 self.addEventListener('install', function(event) {
-    console.log('[SW]: Service worker installing...', event);
+    // var cache = await caches.open('static');
+    // cache.add('/js/app.js');
+    // console.log('->added something to cache');
+    event.waitUntil(
+        caches.open('static')
+            .then(function(cache) {
+                cache.add('/');    
+                cache.add('index.html');    
+                cache.add('/js/app.js');
+                console.log('->added something to cache');
+            })
+    )
     //do not wait for page-reload
     return self.skipWaiting();
 });
 
 self.addEventListener('activate', function(event) {
-    console.log('[SW]: Service worker activating...', event);
     //ask the client to take control over the service worker
     return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-     console.log('[SW]: Service worker fetching...', event);
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(resFromCache) {
+                if(resFromCache) {
+                    return resFromCache;
+                } else {
+                    return fetch(event.request)
+                        .then(function(resFromInternet) {
+                            return caches.open('dynamic')
+                                .then(function(cache) {
+                                    cache.put(event.request.url, resFromInternet.clone())
+                                    return resFromInternet;
+                                });
+                        });
+                }
+            }
+        )
+    )
 });
 
 self.addEventListener('sync', function(event) {
     console.log('[SW]: Service worker syncing...', event);
+
 });
