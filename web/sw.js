a9e841b3e4d0bc0b4a824b7c37abc173a7aac1eb
Stefan Kubinski (LakeDice) <stefan.kubinski@gmail.com>
2023-04-27 16:50:49 +0200

    refactoring

--- web/sw.js
+++ web/sw.js
@@ -1,62 +1,83 @@
-const STATIC_CACHE_NAME = 'static-v2';
-const DYNAMIC_CACHE_NAME = 'dynamic-v2';
+const STATIC_CACHE_NAME = 'static';
+const DYNAMIC_CACHE_NAME = 'dynamic';
+const CACHE_VERSION = "v4";
+const STATIC_CACHE_CONTENT = ['/', 'index.html', 'offline.html', '/js/app.js', 'manifest.json'];
 
-self.addEventListener('install', async function(event) {
-    console.log('[SW]: Service worker installing..., hopefully!', event);
-    //abwarten, bis der gesamte teil innerhalb waitUntil fertig ist
-    //sonst kann fetch möglicherweise noch nicht auf den cache zugreifen
-    // event.waitUntil(
-    //     caches.open('static')
-    //         .then(function(staticCache) {
-    //             staticCache.add('/');
-    //             staticCache.add('index.html');
-    //             staticCache.add('app.js');
-    //         })
-    // );
 
-    var staticCache = await caches.open(STATIC_CACHE_NAME);
-    await staticCache.add('/');
-    await staticCache.add('index.html');
-    await staticCache.add('/js/app.js');
+self.addEventListener('install', event => {
+    event.waitUntil(install());
+  });
+  
+  async function install() {
+    const cache = await caches.open(STATIC_CACHE_NAME + CACHE_VERSION);
+    return cache.addAll(STATIC_CACHE_CONTENT);
+  }
 
-    //do not wait for page-reload
-    //erübrigt die manuelle aktivierung eines neuen serviceworkers
-    return self.skipWaiting();
+self.addEventListener('activate', event => {
+    event.waitUntil(activate());
 });
 
-self.addEventListener('activate', function(event) {
-    //console.log('[SW]: Service worker activating...', event);
-    //ask the client to take control over the service worker
-    return self.clients.claim();
-});
+async function activate() {
+    const keys = await caches.keys();
+    return Promise.all(
+      keys.map(key => {
+        if (!key.endsWith(CACHE_VERSION)) return caches.delete(key);
+      })
+    );
+  }
 
-self.addEventListener('fetch', function(event) {
-    caches.keys()
-        .then(function(cacheEntries) {
-            console.log(cacheEntries);
-        });
-        
-     event.respondWith(
-        caches.match(event.request)
-            .then(function(cacheResponse) {
-                if(cacheResponse) {
-                    //treffer im cache gefunden->zurückgeben
-                    return cacheResponse;
-                } else {
-                    //treffer nicht gefunden->internet fragen
-                    return fetch(event.request)
-                        .then(function(responseFromInternet) {
-                            return caches.open(DYNAMIC_CACHE_NAME)
-                                .then(function(dynamicCache) {
-                                    dynamicCache.put(event.request.url, responseFromInternet.clone());
-                                    return responseFromInternet;
-                                })
-                        })
-                }
-            })
-     )
-});
+self.addEventListener('fetch', event =>
+  event.waitUntil(cacheOnly(event.request))
+);
+
+const networkOnly = async (request) => {
+    return fetch(request);
+  };
+
+const cacheOnly = async (request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      return responseFromCache;
+    }
+  };
+
+const cacheFirstThenNetwork = async (request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      return responseFromCache;
+    }
+
+    try {
+        const responseFromWeb = await fetch(request);
+        return await cacheDynamically(request, responseFromWeb.clone());
+    } catch (error) {
+        return cacheOnly("offline.html");
+    }
+  };
+
+const networkFirstThenCache = async (request) => {
+    try {
+        const responseFromWeb = await fetch(request);
+        return await cacheDynamically(request, responseFromWeb.clone());
+    } catch (error) {
+        const responseFromCache = await caches.match(request);
+        if(responseFromCache) {
+            return responseFromCache;
+        }
+    }
+  };  
+
+const cacheDynamically = async (request, response) => {
+    var responseFromCache = await caches.match(request)
+    if(responseFromCache === null || responseFromCache === undefined) {
+        var dynamicCache = await caches.open(DYNAMIC_CACHE_NAME+CACHE_VERSION);
+        dynamicCache.put(request.url, response);   
+    }
+    return response;
+}
 
 self.addEventListener('sync', function(event) {
     //console.log('[SW]: Service worker syncing...', event);
+
 });
+
